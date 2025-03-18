24135a2e8e6c4c88ef035e3994f23f1c5b67ebb0
Samuel Bello <thesamuelbello@gmail.com>
2025-03-18 22:43:59 +0100

    updated title from "guest" to "away"

--- index.js
+++ index.js
@@ -1,57 +1,57 @@
 // HOME SCORE BOARD
 // HOME BUTTON ONE
-let homeScoreBtnOne = document.getElementById("home-score-btn-1")
-let firstHomeScoreEl =document.getElementById("home-score")
-let homeScore = 0
+let homeScoreBtnOne = document.getElementById("home-score-btn-1");
+let firstHomeScoreEl = document.getElementById("home-score");
+let homeScore = 0;
 
-function increaseHomeScoreOne(){
-    homeScore += 1
-    firstHomeScoreEl.textContent = homeScore
+function increaseHomeScoreOne() {
+  homeScore += 1;
+  firstHomeScoreEl.textContent = homeScore;
 }
 
 // HOME BUTTON TWO
-let homeScoreBtnTwo = document.getElementById("home-score-btn-2")
-let secondHomeScoreEl =document.getElementById("home-score")
+let homeScoreBtnTwo = document.getElementById("home-score-btn-2");
+let secondHomeScoreEl = document.getElementById("home-score");
 
-function increaseHomeScoreTwo(){
-    homeScore += 2
-    secondHomeScoreEl.textContent = homeScore
+function increaseHomeScoreTwo() {
+  homeScore += 2;
+  secondHomeScoreEl.textContent = homeScore;
 }
 
 // HOME BUTTON THREE
-let homeScoreBtnThree = document.getElementById("home-score-btn-3")
-let thirdHomeScoreEl =document.getElementById("home-score")
+let homeScoreBtnThree = document.getElementById("home-score-btn-3");
+let thirdHomeScoreEl = document.getElementById("home-score");
 
-function increaseHomeScoreThree(){
-    homeScore += 3
-    thirdHomeScoreEl.textContent = homeScore
+function increaseHomeScoreThree() {
+  homeScore += 3;
+  thirdHomeScoreEl.textContent = homeScore;
 }
 
 // GUEST SCORE BOARD
 // GUEST BUTTON ONE
-let guestScoreBtnOne = document.getElementById("guest-score-btn-1")
-let firstGuestScoreEl =document.getElementById("guest-score")
-let guestScore = 0
+let guestScoreBtnOne = document.getElementById("guest-score-btn-1");
+let firstGuestScoreEl = document.getElementById("guest-score");
+let guestScore = 0;
 
-function increaseGuestScoreOne(){
-    guestScore += 1
-    firstGuestScoreEl.textContent = guestScore
+function increaseGuestScoreOne() {
+  guestScore += 1;
+  firstGuestScoreEl.textContent = guestScore;
 }
 
 // GUEST BUTTON TWO
-let guestScoreBtnTwo = document.getElementById("guest-score-btn-2")
-let secondGuestScoreEl =document.getElementById("guest-score")
+let guestScoreBtnTwo = document.getElementById("guest-score-btn-2");
+let secondGuestScoreEl = document.getElementById("guest-score");
 
-function increaseGuestScoreTwo(){
-    guestScore += 2
-    secondGuestScoreEl.textContent = guestScore
+function increaseGuestScoreTwo() {
+  guestScore += 2;
+  secondGuestScoreEl.textContent = guestScore;
 }
 
 // GUEST BUTTON THREE
-let guestScoreBtnThree = document.getElementById("guest-score-btn-3")
-let thirdGuestScoreEl =document.getElementById("guest-score")
+let guestScoreBtnThree = document.getElementById("guest-score-btn-3");
+let thirdGuestScoreEl = document.getElementById("guest-score");
 
-function increaseGuestScoreThree(){
-    guestScore += 3
-    thirdGuestScoreEl.textContent = guestScore
+function increaseGuestScoreThree() {
+  guestScore += 3;
+  thirdGuestScoreEl.textContent = guestScore;
 }
